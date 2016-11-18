b3df5f92cada9b89e1a336cb873fdc943fa0168d
Evaristo Gutiérrez <egutierrez@cloudbees.com>
2016-11-18 13:15:12 +0100

    Initialise project containing basic src file and simple test checking the existence of a property.

---
+++ src/main/java/com/cloudbees/manticore/App.java
@@ -0,0 +1,13 @@
+package com.cloudbees.manticore;
+
+/**
+ * Hello world from Maven Basic!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+}
